bb4531ed816b2a988308ca84db0dcb586ac84ec2
YaniqueG <yaniquegayle@hotmail.com>
2022-12-07 15:58:42 +0000

    Instagram Data Modeling Completed

--- src/models.py
+++ src/models.py
@@ -8,23 +8,45 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer,ForeignKey('Comment.author_id'),ForeignKey('Post.user_id'),primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250), unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = "Post"
+    id = Column(Integer,ForeignKey('Comment.post_id'),primary_key=True)
+    user_id =Column(Integer,ForeignKey('User.id'))
+
+class Media(Base):
+    __tablename__ = 'Media'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    media_type = Column(Integer)
+    url = Column(String(250))
+    post_id = Column(String(250),ForeignKey('Post.id'))
+
+class Follower(Base):
+    __tablename__ = 'Follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer,ForeignKey('User.id'))
+    user_to_id = Column(Integer,ForeignKey('User.id'))
+    
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(String(250),primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer)
+    post_id = Column(Integer)
+
+
+
+
+    
+
+
 
     def to_dict(self):
         return {}
